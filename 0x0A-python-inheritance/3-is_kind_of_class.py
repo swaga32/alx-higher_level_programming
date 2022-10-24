3f56fe427bab41a05875c0bf9c2201039adaf9c6
swaga32 <jacksonmbirui3@gmail.com>
2022-10-24 21:26:15 +0000

    juj

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,17 +1,9 @@
-#!usr/bin/python:3
+#!/usr/bin/python3
 """
-Contains method is_kind_of_class
-returns True if object is an instance of class that it inherited from
+Contains the is_kind_of_class function
 """
 
 
 def is_kind_of_class(obj, a_class):
-    """
-    Notes:
-        use type() to get specific class
-        use isinstance() to get class and any parent classes too
-        use issubclass() to get what object is a subclass of
-    Return:
-        True if obj is an instance of class that it inherited from
-    """
-    return isinstance(obj, a_class)
+    """True if obj is an instance or inherited from a_class, else False"""
+    return (isinstance(obj, a_class))
