1426ec109ccecc976cddbe2777649e273d181b36
swaga32 <jacksonmbirui3@gmail.com>
2023-01-17 10:12:33 +0000

    slfd

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+        print("\t- type: <class 'bytes'>$ {}".format(type(content)))
+        print("\t- content: b'OK'$ {}".format(content))
+        print("\t- utf8 content: OK$ {}".format(content.decode('utf-8')))
